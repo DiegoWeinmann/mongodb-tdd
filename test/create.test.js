c2351fef4cad436a49253a9fe3401692acfdefe0
Diego Weinmann <diego.weinmann@hotmail.com>
2020-09-26 20:39:53 -0300

    test: create test suite

--- test/create.test.js
+++ test/create.test.js
@@ -1,7 +1,15 @@
+const { expect } = require('chai')
 const User = require('../src/models/User')
 
 describe('Creating records...', () => {
-  it('saves a user', () => {
-    
-  })
+	it('saves a user', (done) => {
+		const joe = new User({
+			name: 'joe',
+		})
+
+		joe.save().then(() => {
+			expect(joe.isNew).to.be.false
+			done()
+		})
+	})
 })
