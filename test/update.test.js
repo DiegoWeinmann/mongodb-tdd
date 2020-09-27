1466af31cc41097a0e0d04604af50d86e0ac16de
Diego Weinmann <diego.weinmann@hotmail.com>
2020-09-26 21:00:13 -0300

    test: update test suite add it blocks

--- test/update.test.js
+++ test/update.test.js
@@ -3,17 +3,40 @@ const User = require('../src/models/User')
 
 describe('Updating users...', () => {
 	let joe
+	let newName
 
 	beforeEach(async () => {
 		joe = new User({ name: 'Joe' })
+		newName = 'test'
 		return joe.save()
 	})
 
 	it('document set and save', async () => {
-		const newName = 'test'
 		joe.set('name', newName)
 		return joe
 			.save()
+			.then(() => {
+				return User.find({})
+			})
+			.then((users) => {
+				expect(users.length).to.equal(1)
+				expect(users[0].name).to.equal(newName)
+			})
+	})
+
+	it('Model updateOne', () => {
+		return User.updateOne({ name: 'Joe' }, { name: newName })
+			.then(() => {
+				return User.find({})
+			})
+			.then((users) => {
+				expect(users.length).to.equal(1)
+				expect(users[0].name).to.equal(newName)
+			})
+	})
+
+	it('Model findByIdAndUpdate', async () => {
+		return User.findByIdAndUpdate(joe._id, { name: newName })
 			.then(() => {
 				return User.find({})
 			})
