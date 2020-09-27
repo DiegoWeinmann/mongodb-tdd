1c5aa07ad7a572d6e374ea5394dbabf7daeb76c8
Diego Weinmann <diego.weinmann@hotmail.com>
2020-09-27 18:29:20 -0300

    test: association test suite

---
+++ test/association.test.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const { expect } = require('chai')
+
+const User = require('../src/models/User')
+const BlogPost = require('../src/models/BlogPost')
+const Comment = require('../src/models/Comment')
+
+describe('Associations...', () => {})
