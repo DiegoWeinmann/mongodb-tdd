23be952075bb3fedbb2df8c56b9d048f41518afe
Diego Weinmann <diego.weinmann@hotmail.com>
2020-09-27 16:14:24 -0300

    test: change test reporter

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 before((done) => {
+  console.clear()
 	mongoose.connect('mongodb://localhost:27017/users_test', {
 		useUnifiedTopology: true,
 		useNewUrlParser: true,
