e8d74e1909cef2ba1829d628d95d8089f64f6ef3
Diego Weinmann <diego.weinmann@hotmail.com>
2020-09-26 21:41:57 -0300

    feat: add validation to User.name

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-	name: String,
+	name: {
+		type: String,
+		required: [true, 'Name is required.'],
+		validate: {
+			validator: (name) => name.length > 2,
+			message: 'The name must be at least 3 characters long.',
+		},
+	},
+	postCount: Number,
 })
 
 const User = mongoose.model('User', UserSchema)
 
 module.exports = User
-
-
-
